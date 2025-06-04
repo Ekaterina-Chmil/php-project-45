dcd25946355b12fae599d347411ebfd1906713d5
Екатерина Чмиль <chmil.ekat@yandex.ru>
2025-06-04 22:47:05 +0700

    Updated src/Cli.php, Makefile, bin/brain-games

--- Makefile
+++ Makefile
@@ -25,3 +25,11 @@ validate:
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
 
+lint-fix:
+	composer exec --verbose phpcbf -- --standard=PSR12 --colors src bin
+
+dump:
+	composer dump-autoload -o
+
+phpstan:
+	vendor/bin/phpstan analyse
--- src/Cli.php
+++ src/Cli.php
@@ -1,14 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function wellcome()
+function welcome(): string
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
 
