22e1c3227a42818ca10cb78f6216aba2aaf87ad3
Екатерина Чмиль <chmil.ekat@yandex.ru>
2025-07-06 23:01:51 +0700

    Refactor BrainPrime.php and fix bin/brain-prime

--- src/Games/BrainPrime.php
+++ src/Games/BrainPrime.php
@@ -4,30 +4,36 @@ namespace BrainGames\Games\BrainPrime;
 
 use function BrainGames\Engine\runGame;
 
-const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime(int $number): bool
 {
     if ($number < 2) {
         return false;
     }
+
     for ($i = 2; $i <= sqrt($number); $i++) {
         if ($number % $i === 0) {
             return false;
         }
     }
+
     return true;
 }
 
-function generateRound(): array
+function generateData(): array
 {
     $number = rand(2, 100);
     $correctAnswer = isPrime($number) ? 'yes' : 'no';
-    return ["$number", $correctAnswer];
+
+    return [
+	'question' => (string) $number,
+        'correctAnswer' => $correctAnswer,
+    ];
 }
 
-function startGame(): void
+function run(): void
 {
-    runGame(DESCRIPTION, fn() => generateRound());
+    runGame(fn() => generateData(), GAME_DESCRIPTION);
 }
 
